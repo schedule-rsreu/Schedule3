762adb9a4b8b4194a96c39698dff389405b3be67
Andrew <andystep333@gmail.com>
2024-10-22 22:12:12 +0300

    Fixed bugs

--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -9,15 +9,12 @@ import { useCurrentPairIndex } from "../hooks/useCurrentPairIndex";
 
 export const Schedule: React.FC = () => {
   const [schedule, setSchedule] = React.useState<IPair[]>([]);
-  const [isScheduleLoaded, setIsScheduleLoaded] =
-    React.useState<boolean>(false);
+  const [isScheduleLoaded, setIsScheduleLoaded] = React.useState(false);
   const [currentTime, setCurrentTime] = React.useState(
     format(new Date(), "HH:mm")
   );
 
-  const initDataRaw = useStore((state) => state.initDataRaw);
-  const initSchedule = useStore((state) => state.initSchedule);
-  const groupAuth = useStore((state) => state.groupAuth);
+  const prevScheduleRef = React.useRef<IPair[]>([]);
 
   const selectedGroup = useStore((state) => state.selectedGroup);
   const selectedDayEn = useStore((state) => state.selectedDayEn);
@@ -27,13 +24,9 @@ export const Schedule: React.FC = () => {
   const currentDay = useStore((state) => state.currentDay);
   const currentWeek = useStore((state) => state.currentWeek);
 
-  const currentPairIndex = useCurrentPairIndex(
-    schedule,
-    currentTime,
-    selectedDayEn,
-    currentDay,
-    currentWeek
-  );
+  const initSchedule = useStore((state) => state.initSchedule);
+  const groupAuth = useStore((state) => state.groupAuth);
+  const initDataRaw = useStore((state) => state.initDataRaw);
 
   const { data, isLoading, error } = useSchedule(
     selectedGroup
@@ -44,39 +37,52 @@ export const Schedule: React.FC = () => {
     initDataRaw
   );
 
-  React.useEffect(() => {
-    if (selectedGroup && selectedDayEn && selectedWeekEn) {
-      let newSchedule: IPair[] = [];
-      if (data && selectedGroup.value !== groupAuth) {
-        newSchedule =
-          data[selectedWeekEn as keyof ISchedule]?.[
-            selectedDayEn as keyof IDays
-          ] || [];
-      } else if (initSchedule && selectedGroup.value === groupAuth) {
-        newSchedule =
-          initSchedule[selectedWeekEn as keyof ISchedule]?.[
-            selectedDayEn as keyof IDays
-          ] || [];
-      }
+  const currentPairIndex = useCurrentPairIndex(
+    schedule,
+    currentTime,
+    selectedDayEn,
+    currentDay,
+    currentWeek
+  );
 
-      if (
-        (JSON.stringify(newSchedule) !== JSON.stringify(schedule) ||
-          (newSchedule.length === 0 && schedule.length === 0)) &&
-        !isLoading
-      ) {
-        setSchedule(newSchedule);
-        setIsScheduleLoaded(true);
-      }
+  const newSchedule = React.useMemo(() => {
+    if (data && selectedGroup && selectedGroup.value !== groupAuth) {
+      return (
+        data[selectedWeekEn as keyof ISchedule]?.[
+          selectedDayEn as keyof IDays
+        ] || []
+      );
+    } else if (
+      initSchedule &&
+      selectedGroup &&
+      selectedGroup.value === groupAuth
+    ) {
+      return (
+        initSchedule[selectedWeekEn as keyof ISchedule]?.[
+          selectedDayEn as keyof IDays
+        ] || []
+      );
     }
+    return [];
   }, [
     data,
-    isLoading,
     initSchedule,
+    selectedGroup,
     selectedDayEn,
     selectedWeekEn,
-    selectedGroup,
     groupAuth,
   ]);
+
+  React.useEffect(() => {
+    if (
+      JSON.stringify(newSchedule) !== JSON.stringify(prevScheduleRef.current) ||
+      (newSchedule.length === 0 && prevScheduleRef.current.length === 0)
+    ) {
+      prevScheduleRef.current = newSchedule;
+      setSchedule(newSchedule);
+      setIsScheduleLoaded(true);
+    }
+  }, [newSchedule]);
 
   React.useEffect(() => {
     if (
@@ -94,48 +100,48 @@ export const Schedule: React.FC = () => {
     }
   }, [schedule, currentDay, currentWeek]);
 
+  if (isLoading) {
+    return <div className="loader" />;
+  }
+
+  if (error) {
+    return (
+      <div className="flex w-full justify-center py-[1.5rem] px-[1rem] rounded-[1rem] bg-primary mb-[.5rem] no-schedule">
+        <h2 className="font-normal text-[1.5rem]">
+          Ошибка загрузки расписания
+        </h2>
+      </div>
+    );
+  }
+
+  if (!selectedFaculty || !selectedGroup) {
+    return (
+      <div className="flex w-full justify-center py-[1.5rem] px-[1rem] rounded-[1rem] bg-primary mb-[.5rem]">
+        <h2 className="font-normal text-[1.5rem]">
+          Выберите факультет и группу
+        </h2>
+      </div>
+    );
+  }
+
+  if (isScheduleLoaded && schedule.length === 0) {
+    return (
+      <div className="flex w-full justify-center py-[1.5rem] px-[1rem] rounded-[1rem] bg-primary mb-[.5rem] no-schedule">
+        <h2 className="font-normal text-[1.5rem]">В этот день нет занятий</h2>
+      </div>
+    );
+  }
+
   return (
     <main className="flex flex-col justify-center items-center w-full max-w-3xl">
-      {isLoading && <div className="loader" />}
-      {!isLoading && error && (
-        <div className="flex w-full justify-center py-[1.5rem] px-[1rem] rounded-[1rem] bg-primary mb-[.5rem] no-schedule">
-          <h2 className="font-normal text-[1.5rem]">
-            Ошибка загрузки расписания
-          </h2>
-        </div>
-      )}
-      {(!selectedFaculty || !selectedGroup) && !isLoading && !error && (
-        <div className="flex w-full justify-center py-[1.5rem] px-[1rem] rounded-[1rem] bg-primary mb-[.5rem]">
-          <h2 className="font-normal text-[1.5rem]">
-            Выберите факультет и группу
-          </h2>
-        </div>
-      )}
-      {selectedFaculty &&
-        selectedGroup &&
-        !isLoading &&
-        !error &&
-        isScheduleLoaded &&
-        schedule.length === 0 && (
-          <div className="flex w-full justify-center py-[1.5rem] px-[1rem] rounded-[1rem] bg-primary mb-[.5rem] no-schedule">
-            <h2 className="font-normal text-[1.5rem]">
-              В этот день нет занятий
-            </h2>
-          </div>
-        )}
-      {selectedFaculty &&
-        selectedGroup &&
-        !isLoading &&
-        !error &&
-        schedule.length > 0 &&
-        schedule.map((pair, index) => (
-          <Pair
-            key={index}
-            pair={pair}
-            index={index}
-            currentPairIndex={currentPairIndex}
-          />
-        ))}
+      {schedule.map((pair, index) => (
+        <Pair
+          key={index}
+          pair={pair}
+          index={index}
+          currentPairIndex={currentPairIndex}
+        />
+      ))}
     </main>
   );
 };
